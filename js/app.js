f6ffb1dace9bc98da19feffb2963e9080615693a
RettBehrens <everettbehrens@gmail.com>
2017-07-13 20:57:58 -0600

    adds Card and Deck constructors

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log("linked");
+
+window.onload = function() {
+	console.log("loaded");
+};
+
+function Card(rank, suit, value) {
+	this.rank = name;
+	this.suit = suit;
+	this.value = value;
+}
+
+function Deck() {
+	this.rank = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",];
+	this.suit = ["Hearts","Diamonds","Spades","Clubs",];
+}
